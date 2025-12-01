9e9df5a97a84e740da3e800aa0612287dd2982a3
yoshiijoe <phileas.eloi@gmail.com>
2025-12-01 12:55:33 +0100

    adding newfiles

--- poudelard/univers/personnage.py
+++ poudelard/univers/personnage.py
@@ -1,38 +1,32 @@
+from utils import *
 def initialiser_personnage(nom, prenom, attributs):
-    """
-    Crée et retourne le dictionnaire du personnage.
-    Argent départ : 100.
-    Inventaire et Sortilèges : listes vides.
-    """
     joueur = {
         "Nom": nom,
         "Prenom": prenom,
         "Argent": 100,
         "Inventaire": [],
         "Sortilèges": [],
-        "Attributs": attributs  # C'est un dictionnaire (courage, etc.)
+        "Attributs":
     }
     return joueur
 
+
 def afficher_personnage(joueur):
-    """
-    Affiche toutes les infos du joueur (Nom, Argent, Inventaire, etc.).
-    Astuce : Utiliser une boucle pour parcourir les attributs.
-    """
-    print(f"Profil de {joueur['Prenom']} {joueur['Nom']}")
-    # Afficher le reste...
-    pass
+    print(f" Profil de {joueur['Prenom']} {joueur['Nom']}")
+    print(f"Argent : {joueur['Argent']} galions")
+    print(f"Inventaire : {joueur['Inventaire']}")
+    print(f"Attributs : {joueur['Attributs']}")
+
 
 def modifier_argent(joueur, montant):
-    """
-    Ajoute (ou retire si négatif) le montant à l'argent du joueur.
-    """
-    # Code ici
-    pass
+    montant += joueur["Argent"]
+    return montant
+
+
 
 def ajouter_objet(joueur, cle, objet):
-    """
-    Ajoute un objet à la liste spécifiée par 'cle' ("Inventaire" ou "Sortilèges").
-    """
-    # Vérifier si la clé existe, puis append() l'objet à la liste correspondante
-    pass
+    if cle in joueur and (cle == "Inventaire" or cle == "Sortilège"):
+        joueur[cle].append(objet)
+    else :
+        print(f"Erreur : La catégorie '{cle}' n'existe pas ou n'est pas autorisée.")
+    return joueur
