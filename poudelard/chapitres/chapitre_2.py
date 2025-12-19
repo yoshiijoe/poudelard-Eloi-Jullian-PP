d7f1e3c3af6ae177943d3e9d5d240f05dfc893da
PC <jullianadrien@gmail.com>
2025-12-19 11:42:25 +0100

    chap 2 et 3 revew by adrien

--- poudelard/chapitres/chapitre_2.py
+++ poudelard/chapitres/chapitre_2.py
@@ -7,7 +7,7 @@ def rencontrer_amis(joueur):
     print("Un garçon roux entre dans votre compartiment, l'air amical.")
 
     option_ron = ["Bien sûr, assieds-toi !", "Désolé, je préfère voyager seul."]
-    choix_ron = demander_choix("Salut ! Moi c'est Ron Weasley. Tu veux bien qu'on s'assoie ensemble ?", opt_ron)
+    choix_ron = demander_choix("Salut ! Moi c'est Ron Weasley. Tu veux bien qu'on s'assoie ensemble ?", option_ron)
 
     if choix_ron == option_ron[0]:
         print("Ron sourit : Génial ! Tu verras, Poudlard, c'est incroyable !")
@@ -17,10 +17,10 @@ def rencontrer_amis(joueur):
         joueur['Attributs']['ambition'] += 1
 
     print("\nUne fille entre ensuite, portant déjà une pile de livres.")
-    opt_hermione = ["Oui, j'adore apprendre !", "Euh... non, je préfère l'aventure."]
-    choix_hermione = demander_choix("Je suis Hermione Granger. Vous avez lu 'Histoire de la Magie' ?", opt_hermione)
+    option_ron = ["Oui, j'adore apprendre !", "Euh... non, je préfère l'aventure."]
+    choix_hermione = demander_choix("Je suis Hermione Granger. Vous avez lu 'Histoire de la Magie' ?", option_ron)
 
-    if choix_hermione == opt_hermione[0]:
+    if choix_hermione == option_ron[0]:
         print("Hermione sourit : C'est fascinant, n'est-ce pas ?")
         joueur['Attributs']['intelligence'] += 1
     else:
@@ -28,13 +28,13 @@ def rencontrer_amis(joueur):
         joueur['Attributs']['courage'] += 1
 
     print("\nPuis un garçon blond entre avec un air arrogant.")
-    opt_drago = ["Je lui serre la main.", "Je l'ignore.", "Je lui réponds avec arrogance."]
-    choix_drago = demander_choix("Je suis Drago Malefoy. Mieux vaut bien choisir ses amis, non ?", opt_drago)
+    option_ron = ["Je lui serre la main.", "Je l'ignore.", "Je lui réponds avec arrogance."]
+    choix_drago = demander_choix("Je suis Drago Malefoy. Mieux vaut bien choisir ses amis, non ?", option_ron)
 
-    if choix_drago == opt_drago[0]:
+    if choix_drago == option_ron[0]:
         print("Drago vous regarde avec un air satisfait.")
         joueur['Attributs']['ambition'] += 1
-    elif choix_drago == opt_drago[1]:
+    elif choix_drago == option_ron[1]:
         print("Drago fronce les sourcils, vexé : Tu le regretteras !")
         joueur['Attributs']['loyaute'] += 1
     else:
