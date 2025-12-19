b21cec15f51e8148575ae59a881dac2d260fa857
PC <jullianadrien@gmail.com>
2025-12-19 12:50:16 +0100

    dépôt intermédiaire

---
+++ poudelard/chapitres/chapitre_4.py
@@ -0,0 +1,13 @@
+import random
+from poudelard.utils.input_utils import load_fichier, demander_texte
+from poudelard.univers.maison import actualiser_points_maison, afficher_maison_gagnante
+from poudelard.univers.personnage import afficher_personnage
+
+def creer_equipe(maison, equipe_date, est_joueur=False,joueur=None):
+
+
+    return dico
+
+
+def match_quidditch():
+    pass
