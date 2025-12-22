5ebb20c45127c4ca0e10ab72826125afbfab7ea4
4drien <109029168+Cractonc@users.noreply.github.com>
2025-12-22 01:13:50 +0100

    pass et return dico vide juste pour éviter crash

--- poudelard/chapitres/chapitre_4.py
+++ poudelard/chapitres/chapitre_4.py
@@ -3,11 +3,9 @@ from poudelard.utils.input_utils import load_fichier, demander_texte
 from poudelard.univers.maison import actualiser_points_maison, afficher_maison_gagnante
 from poudelard.univers.personnage import afficher_personnage
 
-def creer_equipe(maison, equipe_date, est_joueur=False,joueur=None):
+def creer_equipe(maison, equipe_date, est_joueur=False, joueur=None):
+    return {}
 
 
-    return dico
-
-
-def match_quidditch():
+def match_quidditch(personnage, maisons):
     pass
