03e3e20ce1567e61f83ff0d2b8c3e1c925cbfe23
yoshiijoe <phileas.eloi@gmail.com>
2026-01-06 18:01:56 +0100

    adding newfiles

--- poudelard/chapitres/chapitre_2.py
+++ poudelard/chapitres/chapitre_2.py
@@ -1,7 +1,6 @@
 from poudelard.utils.input_utils import demander_choix
 from poudelard.univers.personnage import afficher_personnage
 
-
 def rencontrer_amis(joueur):
     print("\n--- Dans le Poudlard Express ---")
     print("Un garçon roux entre dans votre compartiment, l'air amical.")
@@ -11,35 +10,35 @@ def rencontrer_amis(joueur):
 
     if choix_ron == option_ron[0]:
         print("Ron sourit : Génial ! Tu verras, Poudlard, c'est incroyable !")
-        joueur['Attributs']['loyauté'] += 1
+        joueur['Attributs']['loyauté'] = joueur['Attributs']['loyauté'] + 1
     else:
         print("Ron hausse les épaules et va chercher une autre place.")
-        joueur['Attributs']['ambition'] += 1
+        joueur['Attributs']['ambition'] = joueur['Attributs']['ambition'] + 1
 
     print("\nUne fille entre ensuite, portant déjà une pile de livres.")
-    option_ron = ["Oui, j'adore apprendre !", "Euh... non, je préfère l'aventure."]
-    choix_hermione = demander_choix("Je suis Hermione Granger. Vous avez lu 'Histoire de la Magie' ?", option_ron)
+    option_hermione = ["Oui, j'adore apprendre !", "Euh... non, je préfère l'aventure."]
+    choix_hermione = demander_choix("Je suis Hermione Granger. Vous avez lu 'Histoire de la Magie' ?", option_hermione)
 
-    if choix_hermione == option_ron[0]:
+    if choix_hermione == option_hermione[0]:
         print("Hermione sourit : C'est fascinant, n'est-ce pas ?")
-        joueur['Attributs']['intelligence'] += 1
+        joueur['Attributs']['intelligence'] = joueur['Attributs']['intelligence'] + 1
     else:
         print("Hermione fronce les sourcils : Il faudrait pourtant s'y mettre !")
-        joueur['Attributs']['courage'] += 1
+        joueur['Attributs']['courage'] = joueur['Attributs']['courage'] + 1
 
     print("\nPuis un garçon blond entre avec un air arrogant.")
-    option_ron = ["Je lui serre la main.", "Je l'ignore.", "Je lui réponds avec arrogance."]
-    choix_drago = demander_choix("Je suis Drago Malefoy. Mieux vaut bien choisir ses amis, non ?", option_ron)
+    option_drago = ["Je lui serre la main.", "Je l'ignore.", "Je lui réponds avec arrogance."]
+    choix_drago = demander_choix("Je suis Drago Malefoy. Mieux vaut bien choisir ses amis, non ?", option_drago)
 
-    if choix_drago == option_ron[0]:
+    if choix_drago == option_drago[0]:
         print("Drago vous regarde avec un air satisfait.")
-        joueur['Attributs']['ambition'] += 1
-    elif choix_drago == option_ron[1]:
+        joueur['Attributs']['ambition'] = joueur['Attributs']['ambition'] + 1
+    elif choix_drago == option_drago[1]:
         print("Drago fronce les sourcils, vexé : Tu le regretteras !")
-        joueur['Attributs']['loyauté'] += 1
+        joueur['Attributs']['loyauté'] = joueur['Attributs']['loyauté'] + 1
     else:
         print("Drago semble furieux de votre audace.")
-        joueur['Attributs']['courage'] += 1
+        joueur['Attributs']['courage'] = joueur['Attributs']['courage'] + 1
 
 
 def ceremonie_repartition(joueur):
@@ -70,7 +69,7 @@ def ceremonie_repartition(joueur):
         maison = "Serpentard"
 
     print("CHOIXPEAU : 'Difficile... très difficile... Mais je sais !'")
-    print(f"CHOIXPEAU : 'Ce sera... {maison} !'")
+    print("CHOIXPEAU : 'Ce sera... {} !'".format(maison))
 
     joueur["Maison"] = maison
     return maison
@@ -83,6 +82,5 @@ def lancer_chapitre_2(joueur):
     input("--- Appuyez sur ENTREE pour voir votre profil mise à jour ---")
     print("\nVoici votre profil mis à jour :")
     afficher_personnage(joueur)
-    print(f"Maison : {joueur['Maison']}")
+    print("Maison : {}".format(joueur['Maison']))
     return joueur
-    
